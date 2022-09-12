a578eadbc9ffff51150dba27159837c565f9c196
Shengda Ding <dingshengda@gmail.com>
2022-09-12 14:39:47 -0700

    Remove import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 //Exports all handler functions
 import "@polkadot/api-augment";
 export * from './mappings/mappingHandlers'
-import {atob} from 'atob';
 
 if (global.atob === undefined) {
   global.atob = atob;
