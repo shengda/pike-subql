635ae310f01847a06929a8982c3619f7525dea81
Shengda Ding <dingshengda@gmail.com>
2022-09-12 14:27:33 -0700

    Add atob

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 //Exports all handler functions
 import "@polkadot/api-augment";
 export * from './mappings/mappingHandlers'
+import {atob} from 'atob';
+
+if (global.atob === undefined) {
+  global.atob = atob;
+}
